6122c37b15d80b6f84aebd47760fa39e4c570862
Peter Boothe <pboothe@google.com>
2017-09-22 14:33:20 -0400

    Add the dummy typedefs to allow NDT to work without web100 or web10g

---
+++ src/dummytypedefs.h
@@ -0,0 +1,12 @@
+#ifndef SRC_TEST_DUMMYTYPEDEFS_H
+#define SRC_TEST_DUMMYTYPEDEFS_H
+
+typedef int tcp_stat_agent;
+typedef int tcp_stat_connection;
+typedef int tcp_stat_group;
+typedef int tcp_stat_log;
+typedef int tcp_stat_snap;
+
+#define TCP_STAT_NAME "NOWEB100"
+
+#endif
